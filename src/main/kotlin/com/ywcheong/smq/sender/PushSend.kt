8987584688b9a6f21096f408593761a3af8bccb2
ywcheong <tencise@gmail.com>
2025-06-21 20:03:17 +0900

    docs: 기존 API 를 Swagger 를 통해 문서화

--- src/main/kotlin/com/ywcheong/smq/sender/PushSend.kt
+++ src/main/kotlin/com/ywcheong/smq/sender/PushSend.kt
@@ -1,6 +1,9 @@
 package com.ywcheong.smq.sender
 
 import com.ywcheong.smq.domain.*
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
@@ -9,6 +12,7 @@ import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
@@ -18,12 +22,29 @@ data class PushSendRequestDto(
     val topics: List<String>,
 )
 
+@Tag(
+    name = "발신자 API", description = "알림을 발신하기 위한 API 입니다."
+)
 @RestController
 class PushSendController @Autowired constructor(
     private val pushSendService: PushSendService
 ) {
+    @Operation(
+        summary = "알림을 발행",
+        description = """알림의 주제를 구독한 수신자 모두에게 이 알림이 전달됩니다. 주제를 구독한 수신자가 없어도 알림은 발행되지만, 아무도 알림을 받지 않습니다.
+알림의 제목, 본문, 그리고 주제 목록을 지정해서 POST 요청의 Body 에 JSON 형식으로 제공해야 합니다.""",
+        responses = [ApiResponse(
+            responseCode = "202", description = "알림이 성공적으로 접수되었습니다. 수신자가 알림을 실제로 수신하는 것은 비동기적으로 처리됩니다."
+        ), ApiResponse(
+            responseCode = "400", description = "요청이 유효하지 않습니다."
+        ), ApiResponse(
+            responseCode = "500", description = "서버 문제로 알림 접수에 실패했습니다."
+        )]
+    )
     @PostMapping("/send")
-    fun send(pushSendRequestDto: PushSendRequestDto): ResponseEntity<Unit> = when (pushSendService.send(
+    fun send(
+        @RequestBody pushSendRequestDto: PushSendRequestDto
+    ): ResponseEntity<Unit> = when (pushSendService.send(
         title = PushTitle(pushSendRequestDto.title),
         body = PushBody(pushSendRequestDto.body),
         topics = pushSendRequestDto.topics.map {
