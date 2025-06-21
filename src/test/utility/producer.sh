38a09e534de08ce5d5110ee1adc6d000d4dcebf2
ywcheong <tencise@gmail.com>
2025-06-21 20:59:02 +0900

    fix: 뒤바뀐 이름 수정

--- src/test/utility/producer.sh
+++ src/test/utility/producer.sh
@@ -1,2 +1,2 @@
 docker exec -it smq-broker-kafka /bin/bash
-/opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic smq-topic
+/opt/kafka/bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic smq-topic
